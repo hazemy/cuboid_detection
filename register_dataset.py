82316e137e49a2574825e03605fe7b2d1b3fe0ab
hazem <hazem.youssef@tu-dortmund.de>
2020-07-13 00:11:42 +0200

    registers test, val, and train datasets to detectron2

--- register_dataset.py
+++ register_dataset.py
@@ -9,6 +9,7 @@ Created on Sun Jul  5 04:22:26 2020
 from detectron2.data import DatasetCatalog, MetadataCatalog
 # from mat2cuboid_annot import mat2cuboid_annot
 from cuboid_annot2detectron_format import cuboid_annot2detectron_format
+from dataset_splitter import split_dataset
 # import os
 import cv2
 import random
@@ -28,14 +29,21 @@ from detectron2.utils.visualizer import Visualizer
 
 # if __name__ == '__main__':
 dataset_dir = '/home/porthos/masters_thesis/datasets/data_release/data_release/cuboid'
-# cuboid_annot = mat2cuboid_annot(os.path.join(dataset_dir, 'Annotations.mat')) 
-# dataset_list = cuboid_annot2detectron_format(cuboid_annot, dataset_dir)
-# for d in ['train']: #ToDo: add validation
+dataset_list = cuboid_annot2detectron_format(dataset_dir)
+train_dataset, val_dataset, test_dataset = split_dataset(dataset_list, 0.6, 0.2)
 # DatasetCatalog.register('cuboid_dataset' + '_train', lambda: cuboid_annot2detectron_format(dataset_dir + '/train'))
-DatasetCatalog.register('cuboid_dataset' + '_train', lambda: cuboid_annot2detectron_format(dataset_dir))
+
+DatasetCatalog.register('cuboid_dataset' + '_train', lambda: train_dataset)
 MetadataCatalog.get('cuboid_dataset' + '_train').set(thing_classes=['cuboid'])    
-cuboid_metadata = MetadataCatalog.get("cuboid_train")
-   
+cuboid_metadata_train = MetadataCatalog.get("cuboid_dataset_train")
+
+DatasetCatalog.register('cuboid_dataset' + '_val', lambda: val_dataset)
+MetadataCatalog.get('cuboid_dataset' + '_val').set(thing_classes=['cuboid'])    
+cuboid_metadata_val = MetadataCatalog.get("cuboid_dataset_val")
+  
+DatasetCatalog.register('cuboid_dataset' + '_test', lambda: test_dataset)
+MetadataCatalog.get('cuboid_dataset' + '_test').set(thing_classes=['cuboid'])    
+cuboid_metadata_test = MetadataCatalog.get("cuboid_dataset_test") 
     
 # DatasetCatalog.register('train', lambda: get_kitti_data_dict(dataset_root_dir, 'train'))
     
@@ -44,19 +52,22 @@ cuboid_metadata = MetadataCatalog.get("cuboid_train")
 # MetadataCatalog.get(d).thing_classes = ["person"]
 
 
-dataset_dicts = cuboid_annot2detectron_format(dataset_dir)
-# sample = random.sample(dataset_dicts, 3)
-i = 0
-for d in dataset_dicts[:4]:
-    img = cv2.imread(d["file_name"])
-    visualizer = Visualizer(img[:, :, ::-1], metadata=cuboid_metadata, scale=1)
-    out = visualizer.draw_dataset_dict(d)
-    # out = visualizer.draw_instance_predictions(dataset_dicts["annotations"].to("cpu"))
-    final_img = cv2.resize(out.get_image()[:, :, ::-1], (500,500))
-    cv2.imshow('2D BB' + str(i), final_img)
-    i=i+1
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+# dataset_dicts = cuboid_annot2detectron_format(dataset_dir)
+# # sample = random.sample(dataset_dicts, 3)
+# i = 0
+
+
+
+# for d in random.sample(DatasetCatalog.get('cuboid_dataset_train'), 5):
+#     img = cv2.imread(d['file_name'])
+#     visualizer = Visualizer(img[:, :, ::-1], metadata=cuboid_metadata_train, scale=1)
+#     out = visualizer.draw_dataset_dict(d)
+#     # out = visualizer.draw_instance_predictions(dataset_dicts["annotations"].to("cpu"))
+#     final_img = cv2.resize(out.get_image()[:, :, ::-1], (500,500))
+#     cv2.imshow('2D BB', final_img)
+#     # i=i+1
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
     
     
     
