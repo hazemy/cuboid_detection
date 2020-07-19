589b10f48a47ff6cf63e312cb9add17da1cbfd8a
hazem <hazem.youssef@tu-dortmund.de>
2020-07-19 21:50:14 +0200

    initial commit

---
+++ search_module.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 19 20:28:58 2020
+
+@author: porthos
+"""
+
+
+def get_2d_annot(image_id):
+    pass
+
+def get_3d_mat_annot(image_id):
+    pass
+
+def get_3d_annot(image_id):
+    pass
+
+
+
+if __name__=='__main__':
+    image_id = '00000005'
+    get_2d_annot(image_id)
