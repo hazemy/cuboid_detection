a536d1d1d80ba6ec52bbda90361eccd590638249
hazem <hazem.youssef@tu-dortmund.de>
2020-07-21 22:57:51 +0200

    called evaluator during training to find AP by overriding DefaultTrainer() method

--- training.py
+++ training.py
@@ -18,17 +18,18 @@ import os
 # from detectron2.data import DatasetCatalog, MetadataCatalog
 # import random, cv2
 
-# from detectron2.evaluation import COCOEvaluator, inference_on_dataset
+from detectron2.evaluation import COCOEvaluator, inference_on_dataset
 # from detectron2.data import build_detection_test_loader
 
+# from MyTrainer import MyTrainer
 
 #training
 def do_training(train):
     cfg = get_cfg()
     cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml"))
     cfg.DATASETS.TRAIN = ("cuboid_dataset_train",)
-    cfg.DATASETS.TEST = () #used to obtain validation loss during training - do Not remove
-    # cfg.TEST.EVAL_PERIOD = 100 #number of iterations at which evaluation is run (to obtain validation loss)
+    cfg.DATASETS.TEST = ("cuboid_dataset_val",) #used to obtain validation loss during training - do Not remove
+    cfg.TEST.EVAL_PERIOD = 100 #number of iterations at which evaluation is run (to obtain validation loss) - It calls the evaluator, if specified
     cfg.DATALOADER.NUM_WORKERS = 2 #number of dataloading threads   
     cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml")  # Let training initialize from model zoo
     cfg.SOLVER.IMS_PER_BATCH = 2
@@ -38,15 +39,36 @@ def do_training(train):
     cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (cuboid)
     cfg.DATALOADER.FILTER_EMPTY_ANNOTATIONS = False #False -> images without annotation are Not removed during training
     os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
-    trainer = DefaultTrainer(cfg) 
-    # trainer.test(evaluator=)
-    if train:
-        trainer.resume_or_load(resume=False)
-        trainer.train() 
-    else:
-        trainer.resume_or_load(resume=True)
-    return trainer, cfg
+    # trainer = DefaultTrainer(cfg)  
+    my_trainer = MyTrainer(cfg)
+    # evaluator = COCOEvaluator("cuboid_dataset_val", cfg, True, output_dir="./output/")
+    # trainer.test(cfg=cfg, model=trainer.model, evaluators=[evaluator])
 
+    if train:
+        # trainer.resume_or_load(resume=False)
+        # trainer.train() 
+        my_trainer.resume_or_load(resume=False)
+        my_trainer.train() 
+    else:
+        # trainer.resume_or_load(resume=True)
+        my_trainer.resume_or_load(resume=True)
+    # data_loader = build_detection_test_loader(cfg, "cuboid_dataset_val")
+    # print(inference_on_dataset(trainer.model, data_loader, evaluator))
+    return my_trainer, cfg
+
+
+class MyTrainer(DefaultTrainer):
+    '''
+    subclass of DefaultTrainer class
+    '''
+    @classmethod
+    def build_evaluator(cls, cfg, dataset_name, output_folder=None):
+        if output_folder is None:
+            output_folder = os.path.join(cfg.OUTPUT_DIR, "inference")
+        return COCOEvaluator(dataset_name, cfg, True, output_folder)
+    
+    
+    
 # cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
 # cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7   # set a custom testing threshold for this model
 # cfg.DATASETS.TEST = ("cuboid_dataset_val",)
