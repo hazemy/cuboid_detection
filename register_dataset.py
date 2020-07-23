9a2d5d93c6cf296ceb304474ebea2b01db8c0519
hazem <hazem.youssef@tu-dortmund.de>
2020-07-23 02:16:31 +0200

    merging json annotation files before registering

--- register_dataset.py
+++ register_dataset.py
@@ -9,28 +9,32 @@ Created on Sun Jul  5 04:22:26 2020
 from detectron2.data import DatasetCatalog, MetadataCatalog
 # from mat2cuboid_annot import mat2cuboid_annot
 # from cuboid_annot2detectron_format import cuboid_annot2detectron_format
-from json2detectron_format import json2detectron_format
+from json2detectron_format import convert2detectron_format
 from dataset_splitter import split_dataset
+from annot_processor import check_missing, merge_annot_files
 # import os
 import cv2
 import random
 from detectron2.utils.visualizer import Visualizer
+import warnings
 
 
 
+annot_files_dir_list = []
+annot_file_dir_1 = '/home/porthos/masters_thesis/datasets/full_dataset/state_hazem.json'
+annot_file_dir_2 = '/home/porthos/masters_thesis/datasets/full_dataset/state_mojtaba.json'   
+annot_file_dir_3= '/home/porthos/masters_thesis/datasets/full_dataset/state_frederick.json'   
+annot_file_dir_4 = '/home/porthos/masters_thesis/datasets/full_dataset/state_ammar.json'   
+annot_files_dir_list = [annot_file_dir_1, annot_file_dir_2, annot_file_dir_3, annot_file_dir_4]
+annot_files_merged = merge_annot_files(annot_files_dir_list)
 
-# def register_dataset(dataset_name, func):
-#     '''
-#     inputs: -dataset in detectron 2 standard format (list of dicts)
-#             -desired name to register dataset with (can be different from original dataset name)
-#     '''
-#     DatasetCatalog.register(dataset_name + '_train', lambda: cuboid_annot2detectron_format(dataset_dir + '/train'))
+# annot_file_all = '/home/porthos/masters_thesis/datasets/full_dataset/state_all.json'
+images_dir = '/home/porthos/masters_thesis/datasets/full_dataset/images'
+missing_list = check_missing(annot_files_merged, images_dir)
+if len(missing_list) > 0:
+    warnings.warn('Dataset Incomplete!')
 
-    
-
-annot_file = '/home/porthos/masters_thesis/datasets/partial_dataset/state_partial.json'
-
-dataset_list = json2detectron_format(annot_file)
+dataset_list = convert2detectron_format(annot_files_merged, images_dir)
 train_dataset, val_dataset, test_dataset = split_dataset(dataset_list, 0.6, 0.2)
 # DatasetCatalog.register('cuboid_dataset' + '_train', lambda: cuboid_annot2detectron_format(dataset_dir + '/train'))
 
