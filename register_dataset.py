2e985f03ab61a1a2d62b1a3019857c5076c76bfb
hazem <hazem.youssef@tu-dortmund.de>
2020-08-02 01:59:50 +0200

    annotation file path modification

--- register_dataset.py
+++ register_dataset.py
@@ -11,30 +11,25 @@ from detectron2.data import DatasetCatalog, MetadataCatalog
 # from cuboid_annot2detectron_format import cuboid_annot2detectron_format
 from format_converter import convert2detectron_format
 from dataset_splitter import split_dataset
-from annot_processor import check_missing, merge_annot_files
-# import os
-import cv2
-import random
-from detectron2.utils.visualizer import Visualizer
-import warnings
+from annot_processor import merge_annot_files, filter_duplicates
 
 
 
-annot_files_dir_list = []
-annot_file_dir_1 = '/home/porthos/masters_thesis/datasets/full_dataset/state_hazem.json'
-annot_file_dir_2 = '/home/porthos/masters_thesis/datasets/full_dataset/state_mojtaba.json'   
-annot_file_dir_3= '/home/porthos/masters_thesis/datasets/full_dataset/state_frederick.json'   
-annot_file_dir_4 = '/home/porthos/masters_thesis/datasets/full_dataset/state_ammar.json'   
-annot_files_dir_list = [annot_file_dir_1, annot_file_dir_2, annot_file_dir_3, annot_file_dir_4]
+# annot_files_dir_list = []
+# annot_file_dir_1 = '/home/porthos/masters_thesis/datasets/full_dataset/state_hazem.json'
+# annot_file_dir_2 = '/home/porthos/masters_thesis/datasets/full_dataset/state_mojtaba.json'   
+# annot_file_dir_3= '/home/porthos/masters_thesis/datasets/full_dataset/state_frederick.json'   
+# annot_file_dir_4 = '/home/porthos/masters_thesis/datasets/full_dataset/state_ammar.json'   
+# annot_files_dir_list = [annot_file_dir_1, annot_file_dir_2, annot_file_dir_3, annot_file_dir_4]
+# annot_files_merged = merge_annot_files(annot_files_dir_list)
+
+annot_file_dir = '/home/porthos/masters_thesis/datasets/full_dataset/state_all_revised.json'
+images_dir = '/home/porthos/masters_thesis/datasets/full_dataset/images'
+annot_files_dir_list = [annot_file_dir]
 annot_files_merged = merge_annot_files(annot_files_dir_list)
 
-# annot_file_all = '/home/porthos/masters_thesis/datasets/full_dataset/state_all.json'
-images_dir = '/home/porthos/masters_thesis/datasets/full_dataset/images'
-missing_list = check_missing(annot_files_merged, images_dir)
-# if len(missing_list) > 0:
-#     warnings.warn('Dataset Incomplete!')
-
-dataset_list = convert2detectron_format(annot_files_merged, images_dir)
+annot_file_filtered = filter_duplicates(annot_files_merged)
+dataset_list = convert2detectron_format(annot_file_filtered, images_dir)
 train_dataset, val_dataset, test_dataset = split_dataset(dataset_list, 0.6, 0.2)
 # DatasetCatalog.register('cuboid_dataset' + '_train', lambda: cuboid_annot2detectron_format(dataset_dir + '/train'))
 
