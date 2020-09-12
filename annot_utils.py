75259920ea98956369e603bdc4a016d2c219e98a
hazem <hazem.youssef@tu-dortmund.de>
2020-09-12 16:17:59 +0200

    conversion of images to jpg format added

---
+++ annot_utils.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 10 15:32:59 2020
+
+@author: porthos
+"""
+
+import os
+from PIL import Image 
+    
+    
+def convert2jpg(images_dir):
+    '''
+    Converts images from png to jpg. Transparent png images are converted to jpg with white background
+    '''
+    for filename in os.listdir(images_dir):
+         if filename.endswith(".png"): 
+             im_path = os.path.join(images_dir, filename)
+             im = Image.open(im_path)
+             im.load()
+             if im.mode != 'RGBA': # 'A' for alpha channel that encodes transparency
+                 im = im.convert('RGBA')
+             background = Image.new("RGB", im.size, (255, 255, 255))
+             background.paste(im, mask=im.split()[3]) # 3 is the alpha channel
+             mod_im_path = os.path.join(images_dir, filename.split('.')[-2] + '.jpg')
+             background.save(mod_im_path, 'JPEG', quality=80)
+             os.remove(im_path)
+                 
+
+
+if __name__ == '__main__':
+    images_dir = '/home/porthos/masters_thesis/datasets/augmented_dataset/images' 
+    convert2jpg(images_dir)
