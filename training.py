d0bbb9e7320d536dd0c1bb10fab1747bccc93f46
hazem <hazem.youssef@tu-dortmund.de>
2020-09-13 01:37:37 +0200

    minor mod

--- training.py
+++ training.py
@@ -27,7 +27,7 @@ def do_training(train):
     cfg.merge_from_file(model_zoo.get_config_file("COCO-Keypoints/keypoint_rcnn_R_101_FPN_3x.yaml"))
     cfg.DATASETS.TRAIN = ("cuboid_dataset_train",)
     cfg.DATASETS.TEST = ("cuboid_dataset_val",) #used to obtain validation loss during training - do Not remove
-    # cfg.TEST.EVAL_PERIOD = 100 #number of iterations at which evaluation is run (to obtain validation loss) - It calls the evaluator, if specified
+    cfg.TEST.EVAL_PERIOD = 100 #number of iterations at which evaluation is run (to obtain validation loss) - It calls the evaluator, if specified
     cfg.DATALOADER.NUM_WORKERS = 4 #number of dataloading threads   
     # cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml")  # Let training initialize from model zoo
     cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Keypoints/keypoint_rcnn_R_101_FPN_3x.yaml")  # Let training initialize from model zoo
@@ -35,7 +35,7 @@ def do_training(train):
     cfg.TEST.KEYPOINT_OKS_SIGMAS = [0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.6]#same reason as for NUM_KEYPOINTS but for the evaluation part
     cfg.SOLVER.IMS_PER_BATCH = 4
     cfg.SOLVER.BASE_LR = 0.0001 #0.00025 
-    cfg.SOLVER.MAX_ITER = 3300  #300 iterations sufficient for mini dataset
+    cfg.SOLVER.MAX_ITER = 7000  #300 iterations sufficient for mini dataset
     cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 512 #128 #number of ROIs to sample for training Fast RCNN head. sufficient for mini dataset (default: 512)
     cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (cuboid)
     cfg.DATALOADER.FILTER_EMPTY_ANNOTATIONS = True #False -> images without annotation are Not removed during training
@@ -58,11 +58,11 @@ class MyTrainer(DefaultTrainer):
     '''
     subclass of DefaultTrainer class
     '''
-    # @classmethod
-    # def build_evaluator(cls, cfg, dataset_name, output_folder=None):
-    #     if output_folder is None:
-    #         output_folder = os.path.join(cfg.OUTPUT_DIR, "inference")
-    #     return COCOEvaluator(dataset_name, cfg, True, output_folder)
+    @classmethod
+    def build_evaluator(cls, cfg, dataset_name, output_folder=None):
+        if output_folder is None:
+            output_folder = os.path.join(cfg.OUTPUT_DIR, "inference")
+        return COCOEvaluator(dataset_name, cfg, True, output_folder)
     
     def build_hooks(self):
         hooks = super().build_hooks()
