a1008d35ec7ac0854209e9d3ea67dd61065d9c5a
hazem <hazem.youssef@tu-dortmund.de>
2020-09-17 11:49:38 +0200

    addded plotting val and total losses

---
+++ plot_train_val.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 14 02:09:41 2020
+
+@author: porthos
+"""
+
+
+import json
+import matplotlib.pyplot as plt
+
+experiment_folder = './output'
+
+def load_json_arr(json_path):
+    lines = []
+    with open(json_path, 'r') as f:
+        for line in f:
+            lines.append(json.loads(line))
+    return lines
+
+experiment_metrics = load_json_arr(experiment_folder + '/metrics.json')
+
+# plt.plot(
+#     [x['iteration'] for x in experiment_metrics], 
+#     [x['total_loss'] for x in experiment_metrics])
+# plt.plot(
+#     [x['iteration'] for x in experiment_metrics if 'validation_loss' in x], 
+#     [x['validation_loss'] for x in experiment_metrics if 'validation_loss' in x])
+# plt.legend(['total_loss', 'validation_loss'], loc='upper left')
+# plt.show()
+
+plt.plot(
+    [x['iteration'] for x in experiment_metrics], 
+    [x['total_loss'] for x in experiment_metrics])
+plt.plot(
+    [x['iteration'] for x in experiment_metrics if 'total_val_loss' in x], 
+    [x['total_val_loss'] for x in experiment_metrics if 'total_val_loss' in x])
+plt.legend(['total_loss', 'total_val_loss'], loc='upper left')
+plt.show()
+
