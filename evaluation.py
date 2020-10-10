9b15ad36974b3da6d4d98bac5287625e6de20e7d
hazem <hazem.youssef@tu-dortmund.de>
2020-10-11 00:54:30 +0200

    minor mods

--- evaluation.py
+++ evaluation.py
@@ -7,34 +7,19 @@ Created on Fri Jul 10 01:38:41 2020
 """
 
 
-from detectron2.config import get_cfg
 from detectron2.evaluation import COCOEvaluator, inference_on_dataset
 from detectron2.data import build_detection_test_loader
-from detectron2.engine import DefaultTrainer
-from detectron2 import model_zoo
-import os
 from training import do_training
 
 
 
 
-#evaluation
 def do_evaluation(dataset, trainer, cfg):
-    # cfg = get_cfg()
-    # cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml"))
-    cfg.merge_from_file(model_zoo.get_config_file("COCO-Keypoints/keypoint_rcnn_R_101_FPN_3x.yaml"))
-    cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
     cfg.TEST.KEYPOINT_OKS_SIGMAS = [0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.6] #TODO: tune evaluator
-    # cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (cuboid)
-    # trainer=DefaultTrainer(cfg)
-    # trainer.resume_or_load(resume=True)
-    # output_dir = os.path.join(cfg.OUTPUT_DIR, 'evaluation')
     evaluator = COCOEvaluator(dataset, cfg, False, output_dir="./output/")
-    # evaluator = COCOEvaluator(dataset, cfg, False, output_dir=output_dir)
     data_loader = build_detection_test_loader(cfg, dataset)
     print(inference_on_dataset(trainer.model, data_loader, evaluator)) #takes inputs (through the dataloader-2nd arg), 
                                        #outputs of the trained model(1st arg), and the method evaluation (evaluator-3rd arg)
-
 
 
 if __name__=='__main__':
--- prediction.py
+++ prediction.py
@@ -8,32 +8,20 @@ Created on Fri Jul 10 01:38:41 2020
 
 
 from detectron2.engine import DefaultPredictor
-from detectron2 import model_zoo
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import DatasetCatalog, MetadataCatalog
-# from detectron2.checkpoint import DetectionCheckpointer
-import cv2, os
+import cv2
 from training import do_training
 
 
-from detectron2.config import get_cfg
 
 
 def get_predictor(dataset, cfg):  
-    # cfg = get_cfg()
-    # model_path = cfg.merge_from_file(model_zoo.get_config_file("COCO-Keypoints/keypoint_rcnn_R_101_FPN_3x.yaml"))
-    # model_path = cfg.merge_from_file(model_zoo.get_config_file("COCO-Keypoints/keypoint_rcnn_X_101_32x8d_FPN_3x.yaml"))
-    model_path = cfg.merge_from_file(model_zoo.get_config_file("COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x.yaml"))
-    # model_path = cfg.merge_from_file(model_zoo.get_config_file("Misc/scratch_mask_rcnn_R_50_FPN_3x_gn.yaml"))
-    cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
-    # cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
     cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7 #custom testing threshold for model
     cfg.DATASETS.TEST = dataset
     predictor = DefaultPredictor(cfg)
     return predictor
 
-
-#prediction / inference (& visualization)
 def do_prediction_and_visualization(dataset, cfg):
     dataset_dicts = DatasetCatalog.get(dataset)
     metadata=MetadataCatalog.get(dataset)
@@ -49,7 +37,6 @@ def do_prediction_and_visualization(dataset, cfg):
                            metadata=metadata,
                            scale=1, 
             )
-            # print(outputs['instances'].pred_classes)
             output_instances = outputs['instances'].to('cpu')
             pred = output_instances.pred_classes
             # print(pred.tolist())
@@ -62,7 +49,6 @@ def do_prediction_and_visualization(dataset, cfg):
             out = v.overlay_instances(boxes=output_instances.pred_boxes, labels=labels, keypoints=output_instances.pred_keypoints)
             final_img = cv2.resize(out.get_image()[:, :, ::-1], (900,900))
             cv2.imshow('Predication:   ' + d['image_id'] + '.jpg', final_img)
-            # cv2.imshow('Predication & GT:   ', final_img)
             k = cv2.waitKey(0)
             if k == 27: #esc key for stop
                 cv2.destroyAllWindows()
@@ -73,10 +59,8 @@ def do_prediction_and_visualization(dataset, cfg):
    
 if __name__=='__main__':
     _, cfg = do_training(train=False)
-    dataset = "cuboid_dataset_val"
-    # dataset = "mock_dataset"
+    dataset = "cuboid_dataset_test"
     do_prediction_and_visualization(dataset, cfg)
-    # do_prediction_and_visualization(dataset)
 
 
 
