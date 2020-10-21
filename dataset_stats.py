1e52e22c3f4611d588faa165b22f5ed696737e53
hazem <hazem.youssef@tu-dortmund.de>
2020-10-21 02:41:22 +0200

    extra plots and stats added

--- dataset_stats.py
+++ dataset_stats.py
@@ -12,7 +12,14 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import json
-
+from matplotlib.ticker import FormatStrFormatter
+from matplotlib import rc
+plt.rcParams['mathtext.fontset'] = 'stix'
+plt.rcParams['font.family'] = 'STIXGeneral'
+# rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+## for Palatino and other serif fonts use:
+#rc('font',**{'family':'serif','serif':['Palatino']})
+rc('text', usetex=True)
 
 
 def get_total (annot_file):
@@ -22,12 +29,14 @@ def get_total (annot_file):
     total_keypts_hid = 0
     total_keypts_out = 0
     instances_num = []
+    annot_time = []
     for entry in annot_file:
         instances = entry['cubes']
         total_ins += len(instances)
         if instances:
             total_pos += 1
             instances_num.append(len(instances))
+            annot_time.append(entry['time'])
             for cube in instances:
                 for keypt in cube:
                     if keypt[2] == 2:
@@ -36,7 +45,7 @@ def get_total (annot_file):
                         total_keypts_hid += 1
                     if keypt[2] == 0:
                         total_keypts_out += 1   
-    return total_pos, total_ins, instances_num, total_keypts_vis, total_keypts_hid, total_keypts_out
+    return total_pos, total_ins, instances_num, total_keypts_vis, total_keypts_hid, total_keypts_out, annot_time
   
     
 def vis_get_total(total_pos, total_ins, instances_num, total_keypts_vis, total_keypts_hid, total_keypts_out):
@@ -48,31 +57,44 @@ def vis_get_total(total_pos, total_ins, instances_num, total_keypts_vis, total_k
     print('Max num of instances per image is: {}'.format(max(instances_num)))
     instances_num_np = np.asarray(instances_num)
     fig_1 = plt.figure()
-    n, bins, _ =  plt.hist(instances_num_np, range(1,11,1), align='left', histtype='bar', ec='black', color='#1F77B4')
+    n, bins, _ =  plt.hist(instances_num_np, bins=range(1,11,1), log=True, align='left', histtype='bar', ec='black', color='#1F77B4')
     # plt.boxplot(instances_num_np)
     plt.xlabel('Number of instances')
     plt.ylabel('Number of images')
     plt.title('Instances per image') 
     fig_1.tight_layout()
-    # plt.show()
-    
+    # plt.savefig('/home/porthos/masters_thesis/writing/figures/dataset/hist_inst_per_img.pdf')
+    plt.show()
+
     # fig_2 = plt.figure()
-    labels = ['Visible', 'Hidden', 'Out']
+    labels = ['Visible (6527)', 'Hidden (1909)', 'Out (216)']
+    vals = [6527, 1909, 216]
+    # pct = ['75.3', '22.3', '2.4']
     data = [total_keypts_vis, total_keypts_hid, total_keypts_out]
     explode = (0.0, 0.0, 0.0)  # only "explode" the 2nd slice
     fig2, ax1 = plt.subplots(subplot_kw=dict(aspect="equal"))
     colors = ['#2CA02C', '#1F77B4', '#D62728']
     wedges, texts, autotexts = ax1.pie(data, explode=explode, labels=labels, 
-                                       autopct='%1.1f%%', textprops=dict(color="w"), 
-                                       shadow=True, startangle=90, colors=colors)
+                                       autopct=make_autopct(vals), 
+                                       textprops=dict(color="w"), shadow=True, startangle=90, 
+                                       colors=colors)
     ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle. 
     ax1.set_title('Visibility of Dataset Keypoints')
     ax1.legend(wedges, labels,
           title="Visibility",
           loc="center left",
           bbox_to_anchor=(1, 0, 0.5, 1))
-    plt.setp(autotexts, size=9, weight="bold")
+    plt.setp(autotexts, size=11, weight="bold")
+    # plt.savefig('/home/porthos/masters_thesis/writing/figures/dataset/vis_pie.pdf')
     plt.show()
+
+
+def make_autopct(values):
+    def my_autopct(pct):
+        total = sum(values)
+        val = int(round(pct*total/100.0))
+        return '{p:.1f} \% '.format(p=pct)
+    return my_autopct
 
 
 def get_mean_pixel(dataset_list):
@@ -123,18 +145,20 @@ def get_areas(dataset_list):
                 ratio = box_area/img_area
                 ratio_img_size.append(ratio)
                 total_ins += 1
-                if ratio < 0.01:
-                    print(entry['file_name']) 
-    return heights_pos, widths_pos, ratio_img_size, areas_imgs_pos, areas, total_ins
+                # if ratio < 0.01:
+                #     print(entry['file_name']) 
+    return heights_pos, widths_pos, heights, widths, ratio_img_size, areas_imgs_pos, areas, total_ins
 
     
-def vis_get_areas(heights_pos, widths_pos, ratio_img_size, areas_imgs_pos, areas, total_ins):
+def vis_get_areas(heights_pos, widths_pos, heights, widths, ratio_img_size, areas_imgs_pos, areas, total_ins):
     print('Min image height is: {}'.format(min(heights_pos)))
     print('Min image width is: {}'.format(min(widths_pos)))
-    print('Min area for image: {}'.format((dataset_list[areas_imgs_pos.index(min(areas_imgs_pos))])['file_name']))
+    # print('Min area for image: {}'.format((dataset_list[areas_imgs_pos.index(min(areas_imgs_pos))])['file_name']))
     print('Max image height is: {}'.format(max(heights_pos)))
     print('Max image width is: {}'.format(max(widths_pos)))
-    print('Max area for image: {}'.format((dataset_list[areas_imgs_pos.index(max(areas_imgs_pos))])['file_name']))
+    # print('Max area for image: {}'.format((dataset_list[areas_imgs_pos.index(max(areas_imgs_pos))])['file_name']))
+    print('Mean image height is: {}'.format(np.mean(np.asarray(heights))))
+    print('Mean image width is: {}'.format(np.mean(np.asarray(widths))))
     ratio_img_size_np = np.asarray(ratio_img_size)
     percent_img_size = np.multiply(ratio_img_size, 100)
     fig_1 = plt.figure()
@@ -142,8 +166,11 @@ def vis_get_areas(heights_pos, widths_pos, ratio_img_size, areas_imgs_pos, areas
                            histtype='bar', ec='black')
     plt.xlabel('Percent of image size')
     plt.ylabel('Number of instances')
-    plt.title('Hisstogtam of Sizes of instances')
+    plt.title('Histogram of instance size')
     fig_1.tight_layout()
+    # plt.savefig('/home/porthos/masters_thesis/writing/figures/dataset/hist_img_size.pdf')
+    plt.show()
+
     # percent_range = np.asarray(range(0,100,10))
     # left_of_first_bin = percent_range.min() - 5
     # right_of_last_bin = percent_range.max() + 5
@@ -154,34 +181,43 @@ def vis_get_areas(heights_pos, widths_pos, ratio_img_size, areas_imgs_pos, areas
     plt.plot(bins[1:], percent_instances, marker='.', markersize=7, color='#1F77B4')
     plt.xlabel('Percent of image size')
     plt.ylabel('Percent of instances')
-    plt.title('Sizes of instances')
+    plt.title('Instance size')
     fig_2.tight_layout()
     
-    fig_3 = plt.figure()
-    n, bins, _ =  plt.hist(areas, bins=10 , log=True, align='left', histtype='bar', ec='black')
-    plt.xlabel('Image area (square pixels)')
-    plt.ylabel('Number of images')
-    plt.title('Histogram of distribution of images areas (log scale)')
+    fig_3, ax_3 = plt.subplots()
+    n, bins, _ = plt.hist(areas, bins=10, log=True, align='mid', histtype='bar', ec='black')
+    ax_3.set_xlabel('Image area (pixels)')
+    ax_3.set_ylabel('Number of images')
+    ax_3.set_title('Area of images')
+    ax_3.set_xticks(np.arange((500000), (14000000), (1000000)))
+    # ax_3.xaxis.set_major_formatter(FormatStrFormatter('%0.0f'))
+    # plt.ticklabel_format(style='sci', axis='x', scilimits=(-2,2))
     fig_3.tight_layout()
+    # plt.savefig('/home/porthos/masters_thesis/writing/figures/dataset/hist_img_area.pdf')
     plt.show()
+
 
 
 def get_annotator_stats(annot_files_dir_list):
     annotator_total_pos = []
     annotator_total_inst = []
+    annotators_time = []
+    annotators_time_combined = []
     for annot_file in annot_files_dir_list:
         with open(annot_file) as f:
             annot_list = json.load(f)
-        total_pos, total_ins, _, _, _, _ = get_total(annot_list)
+        total_pos, total_ins, instances_num, _, _, _, annot_time = get_total(annot_list)
         annotator_total_pos.append(total_pos)
         annotator_total_inst.append(total_ins)
-    return annotator_total_pos, annotator_total_inst
+        annotators_time.append(annot_time)
+        annotators_time_combined.extend(annot_time)
+    return annotator_total_pos, annotator_total_inst, annotators_time, annotators_time_combined
 
 
-def vis_get_annotator_stats(annotator_total_pos, annotator_total_inst):
+def vis_get_annotator_stats(annotator_total_pos, annotator_total_inst, annotators_time, annotators_time_combined):
     print(annotator_total_inst)
     print(annotator_total_pos)
-    labels = ['annot. 1', 'annot. 2', 'annot. 3', 'annot. 4', 'annot. 5']
+    labels = ['Annot. 1', 'Annot. 2', 'Annot. 3', 'Annot. 4', 'Annot. 5']
     x = np.arange(len(labels))
     fig, ax = plt.subplots()
     width = 0.35
@@ -197,7 +233,32 @@ def vis_get_annotator_stats(annotator_total_pos, annotator_total_inst):
     autolabel(ax, rects1)
     autolabel(ax, rects2)
     fig.tight_layout()
+    # plt.savefig('/home/porthos/masters_thesis/writing/figures/dataset/annot_workload.pdf')
     plt.show()
+
+    fig2, ax2 = plt.subplots()
+    ax2.boxplot(annotators_time, 0, '')
+    ax2.set_xticklabels(labels)
+    # fig2.subplots_adjust(left=0.08, right=0.98, bottom=0.05, top=0.9,
+    #                 hspace=0.4, wspace=0.3)
+    ax2.set_title('Annotation time') 
+    ax2.set_xlabel('Annotator number')
+    ax2.set_ylabel('Time (sec)')
+    # plt.savefig('/home/porthos/masters_thesis/writing/figures/dataset/annot_time.pdf')
+    plt.show()
+    
+    fig3, ax3 = plt.subplots()
+    plot_out = ax3.boxplot(annotators_time_combined, 0)
+    ax3.set_xticklabels(labels)
+    total_time = 0
+    output = [item.get_ydata() for item in plot_out['fliers']]
+    outliers = np.asarray(output)[0]
+    for time in annotators_time_combined:
+        # if time not in outliers:
+        total_time += time
+    mean_img_time_all = (total_time)/(len(annotators_time_combined)-len(outliers))
+    print('Total time for all annotators is: {}'.format(total_time))
+    print('Mean image time for all annotators is: {}'.format(mean_img_time_all))
     
 
 def autolabel(ax, rects):
@@ -220,7 +281,7 @@ def get_entry(img_path, dataset_list):
 
 
 if __name__ =='__main__':
-    annot_file_dir_1 = '/home/porthos/masters_thesis/datasets/final_dataset/annotations_hazem.json'
+    annot_file_dir_1 = '/home/porthos/masters_thesis/datasets/final_dataset/annotations_hazem_mod.json'
     annot_file_dir_2 = '/home/porthos/masters_thesis/datasets/final_dataset/annotations_ammar.json'
     annot_file_dir_3 = '/home/porthos/masters_thesis/datasets/final_dataset/annotations_pablo.json'
     annot_file_dir_4 = '/home/porthos/masters_thesis/datasets/final_dataset/annotations_anas.json'
@@ -232,17 +293,17 @@ if __name__ =='__main__':
     annot_file_amended = amend_dir(unique)
     annot_file_corrected = remove_faulty(annot_file_amended)
     
-    total_pos, total_ins, instances_num, total_keypts_vis, total_keypts_hid, total_keypts_out = get_total(annot_file_corrected)
+    total_pos, total_ins, instances_num, total_keypts_vis, total_keypts_hid, total_keypts_out, _ = get_total(annot_file_corrected)
     vis_get_total(total_pos, total_ins, instances_num, total_keypts_vis, total_keypts_hid, total_keypts_out)
     
     dataset_list = convert2detectron_format(annot_file_corrected, images_dir)
     get_mean_pixel(dataset_list)
     
-    heights_pos, widths_pos, ratio_img_size, areas_imgs_pos, areas, total_ins = get_areas(dataset_list)
-    vis_get_areas(heights_pos, widths_pos, ratio_img_size, areas_imgs_pos, areas, total_ins)
+    heights_pos, widths_pos, heights, widths, ratio_img_size, areas_imgs_pos, areas, total_ins = get_areas(dataset_list)
+    vis_get_areas(heights_pos, widths_pos, heights, widths, ratio_img_size, areas_imgs_pos, areas, total_ins)
     
-    annotator_total_pos, annotator_total_inst = get_annotator_stats(annot_files_dir_list)
-    vis_get_annotator_stats(annotator_total_pos, annotator_total_inst)
+    annotator_total_pos, annotator_total_inst, annotators_time, annotators_time_combined = get_annotator_stats(annot_files_dir_list)
+    vis_get_annotator_stats(annotator_total_pos, annotator_total_inst, annotators_time, annotators_time_combined)
     
     img_query = '/home/porthos/masters_thesis/datasets/final_dataset/images/1791076.jpg'
     img_data = get_entry(img_query, dataset_list)
